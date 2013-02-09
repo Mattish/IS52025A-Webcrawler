ffaa8881a01ad62b64790590a70b85216f6a2215
Mattish <Mattish@tentaclenoises.co.uk>
2013-02-09 23:51:29 +0000

    Table - Class for 2D mapping of Integers

---
+++ Table.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class Table{
+	HashMap<Integer,HashMap<Integer,Integer>> map;
+	public Table(){
+		map = new HashMap<Integer,HashMap<Integer,Integer>>();
+	}
+
+	public Integer get(int k1, int k2){
+		HashMap<Integer,Integer> tmpMap = map.get(k1);
+		if (tmpMap != null){
+			Integer result = tmpMap.get(k2);
+			result = result != null ? result : 0;
+			return result;
+		}
+		else{
+			map.put(k1, new HashMap<Integer,Integer>());
+			return 0;
+		}
+	}
+
+	public void put(int k1, int k2, int value){
+		HashMap<Integer,Integer> tmpMap = map.get(k1);
+		if (tmpMap == null)
+			map.put(k1, new HashMap<Integer,Integer>());
+		tmpMap = map.get(k1);
+		tmpMap.put(k2,value);
+	}
+}
